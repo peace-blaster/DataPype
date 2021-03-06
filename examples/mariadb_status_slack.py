9bbe255c1d495a9b785a15d89caf108e356d1190
peaceblaster <53p3n7@gmail.com>
2021-03-06 02:08:53 -0500

    Updated examples

--- examples/mariadb_status_slack.py
+++ examples/mariadb_status_slack.py
@@ -1,6 +1,15 @@
-from DataPype.etc import slack_post as sp
-import subprocess
 
+import DataPype.etc as etc
+import subprocess
+#get mariadb status report to slack:
 mariadb=subprocess.check_output(['systemctl','status','mariadb'], text=True)
-mariadb=str(a[0:-1])
-print(mariadb)
+mariadb=str(mariadb[0:-1])
+#write to Slack:
+mariadb='```'+mariadb+'```'
+etc.slack_post(message=mariadb, channel='sys-status-report', user='sysadmin-bot', webhook='https://hooks.slack.com/services/something')
+#lets check for users too:
+users=subprocess.check_output(['who'], text=True)
+users=str(users[0:-1])
+#write to Slack again:
+users='```'+users+'```'
+etc.slack_post(message=users, channel='sys-status-report', user='sysadmin-bot', webhook='https://hooks.slack.com/services/something')
--- mariadb_status_slack.py
+++
@@ -1,6 +0,0 @@
-from DataPype.etc import slack_post as sp
-import subprocess
-
-mariadb=subprocess.check_output(['systemctl','status','mariadb'], text=True)
-mariadb=str(a[0:-1])
-print(mariadb)
